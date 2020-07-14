7dd82d17b8d21963882854cad510fc92c63acd87
chandu03 <chandragupta.singh4@gmail.com>
2020-07-15 00:01:06 +0530

    assignment-4 task 2,3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,29 +3,30 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import {Loading} from './LoadingComponent'
 import {baseUrl} from '../shared/baseURL'
+import { Fade, Stagger } from 'react-animation-components';
 
 function About(props) {
 
     const leaders = props.leaders.leaders.map((leader) => {
         return (
-            <div key ={leader.id} className="col-12 mt-5">
             <RenderLeader leader={leader} />
-            </div>
         )
     })
 
     function RenderLeader({leader}){
         return(
-                <Media tag="li">
-                    <Media left middle>
-                        <Media object src={baseUrl + leader.image} alt={leader.name} />
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading>{leader.name}</Media>
-                        <p>{leader.designation}</p>
-                        <p>{leader.description}</p>
-                    </Media>
+            <Fade in>
+            <Media tag="li" key={leader.id} className="col-12 mt-5" >
+                <Media left middle>
+                    <Media object src={baseUrl + leader.image} alt={leader.name} />
                 </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+            </Media>
+            </Fade>
             
         )
     }
@@ -42,7 +43,9 @@ function About(props) {
         }
         else return (
             <Media list>
+            <Stagger in>
                 {leaders}
+            </Stagger>
             </Media>
         );
     }
